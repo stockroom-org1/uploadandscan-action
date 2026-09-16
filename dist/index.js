9da6bafe14b10508edad3f45b4e6d835c46cf2b7
Narayanan Chandran <nchandran@veracode.com>
2026-09-16 14:46:36 +0530

    update api gateway

--- dist/index.js
+++ dist/index.js
@@ -93626,8 +93626,8 @@ module.exports = appConfig;
 
 function appConfig() {
   return {
-    us: 'api.veracode.com',
-    eu: 'api.veracode.eu',
+    us: 'api-agora-stage-103.stage.veracode.io',
+    eu: 'api-agora-stage-103.stage.veracode.io',
     policyUri: '/appsec/v1/policies',
     applicationUri: '/appsec/v1/applications',
     findingsUri: '/appsec/v2/applications',
